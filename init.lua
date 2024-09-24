c6131887f2ff0e516ce9146e7f531b43af694526
Ryker Sun <rykersun2002@gmail.com>
2024-09-24 19:58:12 +0800

    add theme

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ vim.opt.smartindent = true  -- 智能縮進
 vim.opt.wrap = false  -- 不自動換行
 vim.opt.ignorecase = true  -- 搜索時忽略大小寫
 vim.opt.smartcase = true  -- 搜索時若包含大寫字母，則區分大小寫
-vim.opt.termguicolors = true  -- 不使用真實色彩
+vim.opt.termguicolors = true  -- 使用真實色彩
 vim.opt.signcolumn = "yes"  -- 永遠顯示符號欄
 vim.opt.updatetime = 250  -- 更新時間為 250 毫秒
 vim.opt.timeoutlen = 300  -- 映射超時時間為 300 毫秒
@@ -89,6 +89,43 @@ require("lazy").setup({
           --indent = { enable = true },  -- 啟用智能縮進
       })
     end,
+  },
+
+  -- VSCode theme
+  {
+    "Mofiqul/vscode.nvim",
+    config = function ()
+        -- For dark theme (neovim's default)
+        vim.o.background = 'dark'
+        -- For light theme
+        --vim.o.background = 'light'
+        local c = require('vscode.colors').get_colors()
+        require('vscode').setup({
+            -- Alternatively set style in setup
+            -- style = 'light'
+            -- Enable transparent background
+            transparent = true,
+            -- Enable italic comment
+            italic_comments = true,
+            -- Underline `@markup.link.*` variants
+            underline_links = true,
+            -- Disable nvim-tree background color
+            disable_nvimtree_bg = true,
+            -- Override colors (see ./lua/vscode/colors.lua)
+            color_overrides = {
+                vscLineNumber = '#FFFFFF',
+            },
+            -- Override highlight groups (see ./lua/vscode/theme.lua)
+            group_overrides = {
+                -- this supports the same val table as vim.api.nvim_set_hl
+                -- use colors from this colorscheme by requiring vscode.colors!
+                Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+            }
+        })
+        -- require('vscode').load()
+        -- load the theme without affecting devicon colors.
+        vim.cmd.colorscheme "vscode"
+    end
   },
 
   -- LSP 設定
